b760df3b8caa11d414317a24050a28764e296153
Olasunkanmi M. Alabi <alabiolasunkanmi69@gmail.com>
2023-07-17 19:29:09 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-entry point into the program
+ * Return:0 if there is no error
+ */
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
